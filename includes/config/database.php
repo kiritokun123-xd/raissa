deb1377f54adee790ca6ef39ca22498b12d05b5f
kiritokun123-xd <david22android@gmail.com>
2023-01-26 14:46:02 -0500

    cambio david

--- includes/config/database.php
+++ includes/config/database.php
@@ -4,7 +4,7 @@ function conectarDB() : mysqli{
     $db = new mysqli("localhost", "root", "root","raissa");
     $db -> set_charset("utf8");
     if(!$db){
-        echo "Error no se pudo conectar";
+        echo "Error ndo se pudo conectar";
         exit;
     }
 
